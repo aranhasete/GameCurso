c819e6851f424202ce2464d93ce2d4ec6f608ad6
aranhasete <aranhajba@gmail.com>
2016-08-29 23:57:22 -0300

    Iniciando UI

---
+++ GameCurso/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class PauseManager : MonoBehaviour {
+	
+	Canvas canvas;
+    Resolution[] resolutions;
+    Dropdown dropdownResolucao;
+    Toggle windowsMode;
+
+    void Start()
+	{
+		canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+
+        resolutions = Screen.resolutions;
+        dropdownResolucao = GameObject.Find("ResolucaoDropdown").GetComponent<Dropdown>();
+        windowsMode = GameObject.Find("WindowsModeToggle").GetComponent<Toggle>();
+
+        dropdownResolucao.options.Clear();
+        foreach (Resolution res in resolutions)
+        {
+            //print(res.width + "x" + res.height);
+            Debug.Log(res.width + "x" + res.height);
+            dropdownResolucao.options.Add(new Dropdown.OptionData(res.width + "x" + res.height));
+        }
+
+    }
+
+    void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Pause();
+		}
+
+        Screen.SetResolution(resolutions[dropdownResolucao.value].width, resolutions[dropdownResolucao.value].height, !windowsMode.isOn);
+    }
+	
+	public void Pause()
+	{
+        canvas.enabled = !canvas.enabled;
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+    }
+
+    public void Quit()
+	{
+		#if UNITY_EDITOR 
+		EditorApplication.isPlaying = false;
+		#else 
+		Application.Quit();
+		#endif
+	}
+}
